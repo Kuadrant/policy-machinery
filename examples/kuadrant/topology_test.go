f2fcb05469612ec2f395e09c056d2ddaf0b7307d
Guilherme Cassolato <guicassolato@gmail.com>
2024-06-27 12:19:45 +0200

    tests: assertions for ColorPolicy effective policies

--- examples/kuadrant/topology_test.go
+++ examples/kuadrant/topology_test.go
@@ -43,7 +43,7 @@ import (
 //	                                                 │ my-service │
 //	                                                 └────────────┘
 func TestMergeBasedOnTopology(t *testing.T) {
-	gateways := []*machinery.Gateway{{Gateway: machinery.BuildGateway()}}
+	gateway := &machinery.Gateway{Gateway: machinery.BuildGateway()}
 	httpRoutes := []*machinery.HTTPRoute{
 		{HTTPRoute: machinery.BuildHTTPRoute(func(r *gwapiv1.HTTPRoute) {
 			r.Name = "my-route-1"
@@ -142,32 +142,85 @@ func TestMergeBasedOnTopology(t *testing.T) {
 	}
 
 	topology := machinery.NewTopology(
-		machinery.WithTargetables(gateways...),
+		machinery.WithTargetables(gateway),
 		machinery.WithTargetables(httpRoutes...),
 		machinery.WithTargetables(httpRouteRules...),
 		machinery.WithTargetables(services...),
 		machinery.WithLinks(
-			machinery.LinkGatewayToHTTPRouteFunc(gateways),
+			machinery.LinkGatewayToHTTPRouteFunc([]*machinery.Gateway{gateway}),
 			machinery.LinkHTTPRouteToHTTPRouteRuleFunc(),
 			machinery.LinkHTTPRouteRuleToServiceFunc(httpRouteRules),
 		),
 		machinery.WithPolicies(policies...),
 	)
 
+	effectivePoliciesByPath := make(map[string]ColorPolicy)
+
 	for _, httpRouteRule := range httpRouteRules {
-		for _, path := range topology.Paths(gateways[0], httpRouteRule) {
-			var emptyPolicy machinery.Policy = buildPolicy()
+		for _, path := range topology.Paths(gateway, httpRouteRule) {
+			// Gather all policies in the path sorted from the least specific (gateway) to the most specific (httprouterule)
+			// Since in this example there are no targetables with more than one policy attached to it, we can safely just
+			// flat the slices of policies; otherwise we would need to ensure that the policies at the same level are sorted
+			// by creationTimeStamp.
 			policies := lo.FlatMap(path, func(targetable machinery.Targetable, _ int) []machinery.Policy {
 				return targetable.Policies()
 			})
+
+			// Map reduces the policies from most specific to least specific, merging them into one effective policy for
+			// each path
+			var emptyPolicy machinery.Policy = buildPolicy()
 			effectivePolicy := lo.ReduceRight(policies, func(effectivePolicy machinery.Policy, policy machinery.Policy, _ int) machinery.Policy {
 				return effectivePolicy.Merge(policy)
 			}, emptyPolicy)
-			// TODO(guicassolato): Assert the effective policy
+
+			pathStr := strings.Join(lo.Map(path, func(t machinery.Targetable, _ int) string { return t.GetName() }), " → ")
+			effectiveColorPolicy := effectivePolicy.(*ColorPolicy)
+			effectivePoliciesByPath[pathStr] = *effectiveColorPolicy.DeepCopy()
+
 			jsonPolicy, _ := json.MarshalIndent(effectivePolicy, "", "  ")
-			fmt.Println("Path:", strings.Join(lo.Map(path, func(t machinery.Targetable, _ int) string { return t.GetName() }), " → "))
-			fmt.Println("Num. of policies:", len(policies))
-			fmt.Println("Effective policy:", string(jsonPolicy))
+			fmt.Printf("Effective policy for path %s:\n%s\n", pathStr, jsonPolicy)
+		}
+	}
+
+	expectedPolicyRulesByPath := map[string]map[string]ColorValue{
+		"my-gateway → my-route-1 → my-route-1#rule-1": {
+			// from house-colors-route-1
+			"walls": Green,
+			"roof":  Orange,
+			// from house-colors-route-1-rule-1
+			"floor": Red,
+		},
+		"my-gateway → my-route-1 → my-route-1#rule-2": {
+			// from house-colors-route-1
+			"walls": Green,
+			"roof":  Orange,
+		},
+		"my-gateway → my-route-2 → my-route-2#rule-1": {
+			// from house-colors-route-2-rule-1
+			"walls": White,
+			"floor": Yellow,
+		},
+		"my-gateway → my-route-2 → my-route-2#rule-2": {
+			// from house-colors-gw
+			"walls": Black,
+			"doors": Blue,
+		},
+	}
+
+	if len(effectivePoliciesByPath) != len(expectedPolicyRulesByPath) {
+		t.Fatalf("expected %d paths, got %d", len(expectedPolicyRulesByPath), len(effectivePoliciesByPath))
+	}
+
+	for path, expectedRules := range expectedPolicyRulesByPath {
+		effectivePolicy := effectivePoliciesByPath[path]
+		effectiveRules := effectivePolicy.Spec.Proper().Rules
+		if len(effectiveRules) != len(expectedRules) {
+			t.Fatalf("expected %d rules for path %s, got %d", len(expectedRules), path, len(effectiveRules))
+		}
+		for _, rule := range effectiveRules {
+			if rule.Color != expectedRules[rule.Id] {
+				t.Errorf("expected rule %s to have color %s for path %s, got %s", rule.Id, expectedRules[rule.Id], path, rule.Color)
+			}
 		}
 	}
 }
