7a51d52a3ad26a204ac208a09ea44afd14940b2f
Guilherme Cassolato <guicassolato@gmail.com>
2024-08-15 09:55:47 -0300

    controller: unit tests
    Signed-off-by: Guilherme Cassolato <guicassolato@gmail.com>

--- controller/types.go
+++ controller/types.go
@@ -14,4 +14,6 @@ var (
 	GatewayClassesResource = gwapiv1.SchemeGroupVersion.WithResource("gatewayclasses")
 	GatewaysResource       = gwapiv1.SchemeGroupVersion.WithResource("gateways")
 	HTTPRoutesResource     = gwapiv1.SchemeGroupVersion.WithResource("httproutes")
+	ServicesResource       = core.SchemeGroupVersion.WithResource("services")
+	ConfigMapsResource     = core.SchemeGroupVersion.WithResource("configmaps")
 )
